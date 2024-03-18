9c84353d79524e5467c532a74b9367aefbe1173e
AGiriGoswami <143499852+AGiriGoswami@users.noreply.github.com>
2024-03-18 20:39:01 +0530

    first commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 const Home = () => {
   const navigate = useNavigate();
