fe1ed1e8503f3e2e9e64cd5c5d8b3d35b5fb63e0
AjayGiriGoswami <153065760+AjayGiriGoswami@users.noreply.github.com>
2024-04-02 10:48:25 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,6 +6,7 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./Contact.css";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
   const [fname, Setfname] = useState("");
@@ -13,6 +14,7 @@ const Contact = () => {
   const [email, Setemail] = useState("");
   const [mobile, Setmobile] = useState("");
   const [message, Setmessage] = useState("");
+  const navigate = useNavigate("");
 
   const sentUserdata = async (e) => {
     e.preventDefault();
@@ -33,45 +35,54 @@ const Contact = () => {
       toast.warning("Invaild Email", {
         position: "top-center",
       });
-    } else {
-      const res = await axios.post(
-        "https://portfoliobackends-6r0d.onrender.com/Contact",
-        {
-          fname,
-          lname,
-          email,
-          mobile,
-          message,
-        }
-      );
-      // console.log(res);
-
-      if (res.status === 201) {
-        toast.success("Response Sent Successfully", {
-          position: "top-center",
-        });
-      } else if (res.data === "existed") {
-        toast.warn("Already Sent a Response", {
-          position: "top-center",
-        });
-      } else if (res.data === "error") {
-        toast.success("Response Sent Successfully", {
-          position: "top-center",
-        });
+    } else if (mobile === "") {
+      toast.error("Please enter the Number", {
+        position: "top-center",
+      });
+    } else if (mobile.length != 10) {
+      toast.error("Please enter the 10 digit Number", {
+        position: "top-center",
+      });
+    }
+    const res = await axios.post(
+      "https://portfoliobackends-6r0d.onrender.com/Contact",
+      {
+        fname,
+        lname,
+        email,
+        mobile,
+        message,
       }
+    );
+
+    if (res.status === 201) {
+      toast.success("Response Sent Successfully", {
+        position: "top-center",
+      });
+      window.location.reload("/");
+    } else if ((res.data = "require")) {
+      toast.info("Please try after Sometime", {
+        position: "top-center",
+      });
+    } else {
+      toast.info("Please try after Sometime", {
+        position: "top-center",
+      });
     }
   };
+
   return (
     <>
       <div className="container mb-3 contact">
         <h2 className="text-center mt-3">Contact US</h2>
         <div className="container mt-2">
-          <Form className="row mt-2">
+          <Form className="row mt-2" onSubmit={sentUserdata}>
             <Form.Group className="mb-3 col-lg-6" controlId="formBasicEmail">
               <Form.Label>First Name</Form.Label>
               <Form.Control
                 type="text"
                 name="fname"
+                value={fname}
                 onChange={(e) => Setfname(e.target.value)}
               />
             </Form.Group>
@@ -80,6 +91,7 @@ const Contact = () => {
               <Form.Control
                 type="text"
                 name="lname"
+                value={lname}
                 onChange={(e) => Setlname(e.target.value)}
               />
             </Form.Group>
@@ -88,6 +100,7 @@ const Contact = () => {
               <Form.Control
                 type="email"
                 name="email"
+                value={email}
                 onChange={(e) => Setemail(e.target.value)}
               />
             </Form.Group>
@@ -96,6 +109,7 @@ const Contact = () => {
               <Form.Control
                 type="text"
                 name="mobile"
+                value={mobile}
                 onChange={(e) => Setmobile(e.target.value)}
               />
             </Form.Group>
@@ -106,18 +120,14 @@ const Contact = () => {
               <Form.Label>Message</Form.Label>
               <Form.Control
                 as="textarea"
+                value={message}
                 rows={4}
                 onChange={(e) => Setmessage(e.target.value)}
                 name="message"
               />
             </Form.Group>
             <div className="d-flex justify-content-center">
-              <Button
-                variant="primary"
-                className="col-lg-6"
-                type="submit"
-                onClick={sentUserdata}
-              >
+              <Button variant="primary" className="col-lg-6" type="submit">
                 Submit <i class="fa-solid fa-paper-plane"></i>
               </Button>
             </div>
@@ -128,5 +138,4 @@ const Contact = () => {
     </>
   );
 };
-
 export default Contact;
